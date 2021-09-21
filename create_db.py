069567444191028137ffb985d8625e1432a47375
Brandon Densmore <densmoreb1@icloud.com>
2021-09-21 09:08:00 -0600

    created database

---
+++ create_db.py
@@ -0,0 +1,14 @@
+import sqlite3
+from sqlite3.dbapi2 import connect
+
+def create_db(sql_file, database_name):
+    sql_script = open(sql_file)
+    connection = sqlite3.connect(database_name)
+    cursor = connection.cursor()
+
+    script_file = sql_script.read()
+
+    cursor.executescript(script_file)
+
+    connection.commit()
+    connection.close()
