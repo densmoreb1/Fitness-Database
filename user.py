170889114c6053940ebd9fa09b387b33f6b5205a
Brandon Densmore <densmoreb1@icloud.com>
2021-09-28 22:01:27 -0600

    added view workout function

--- user.py
+++ user.py
@@ -33,7 +33,7 @@ class user():
             
             if db_user == self.username and db_pass == self.password:
                 print(f'welcome back {self.username}')
-                self.add_workout()
+                self.main_menu()
             else:
                 print('incorrect credentials')
 
@@ -47,7 +47,7 @@ class user():
             self.cursor.execute('INSERT INTO users(user_name, password, first_name, last_name) VALUES (?, ?, ?, ?)', values1)
             self.connection.commit()
             # self.connection.close()
-            self.add_workout()
+            self.main_menu()
 
     
     def add_workout(self):
@@ -76,19 +76,52 @@ class user():
                         choice = input('another exercise? ')
                         choice.lower()
                 else:
-                    self.add_exercise(exercise)
-
-                
+                    print('the exercise does not exist')
+                    self.add_exercise()    
         if choice == 'n':
             print('you are lazy')
 
-    def add_exercise(self, exercise):
-        choice = input('The exercise does not exists currently. Would you like to add it? y or n ')
+
+    def add_exercise(self):
+        choice = input('add an exercise? ')
         choice.lower()
         if choice == 'y':
+            exercise = input('what is the name of the exercise? ')
             descri = input('enter a description: ')
             values = exercise, descri
             self.cursor.execute('INSERT INTO exercises (exercise_name, description) VALUES ?, ?', values)
             self.connection.commit()
         else:
-            print('wrong')
+            self.main_menu()
+
+
+    def view_workout(self):
+        # view exercise_name, sets, reps, weight where the username = 
+        date = input('enter the date of the workout you would like to see' )
+        self.cursor.execute('''SELECT exercise_name, sets, reps, weight 
+            FROM user_workouts 
+                JOIN exercises
+                    ON user_workouts.exercise_id = exercises.exercise_id
+                JOIN users
+                    ON users.user_id = exercises.exercise_id
+            WHERE users.user_id =  ?
+            ''', self.username)
+        print(self.cursor.fetchall())
+
+    def main_menu(self):
+        # option of viewing, adding workout or exercise
+        choice = input('what would you like to do?: view workout(v), add workout(w), add exercise(e), or quit(q)')
+        while choice != 'q':
+            if choice == 'v':
+                self.view_workout()
+            elif choice == 'w':
+                self.add_workout()
+            elif choice == 'e':
+                self.add_exercise()
+            elif choice == 'q':
+                break
+            else:
+                print('must choose v, w, e, or q')
+
+
+
