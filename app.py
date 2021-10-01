bf968cd5782b5626b62f20223db824cb8333513d
Brandon Densmore <densmoreb1@icloud.com>
2021-10-01 08:48:07 -0600

    add more functionality

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
 from user import user
-import sqlite3
-
 
 # login or create user
 user = user()
-# user.login()
-user.add_exercise()
-# add a workout
-
-# user.add_workout()
-
-# view past workouts
+user.login()
--- user.py
+++ user.py
@@ -1,4 +1,5 @@
 from io import DEFAULT_BUFFER_SIZE
+from os import close
 import sqlite3
 from sqlite3.dbapi2 import Cursor, connect
 
@@ -9,69 +10,85 @@ class user():
         self.username = None
         self.password = None
 
-    def get_credentials(self, values):
+    def get_credentials(self):
         """ This function gets the username and password from the fitness database and returns them """
-
+        values = (self.username,)
         self.cursor.execute('SELECT user_name FROM users WHERE user_name = (?)', values)
         db_user = self.cursor.fetchall()
-        self.cursor.execute('SELECT password FROM users WHERE user_name = (?)', values)
-        db_pass = self.cursor.fetchall()
-        return db_user[0][0], db_pass[0][0]
-
+        if db_user[0][0] == self.username:
+            return True
+        else:
+            print('invalid credentials')
+            
     
     def login(self):
         """ When run, the user will login in with their password and user name"""
-    
-        choice = input('login or create user ')
-        choice.lower()
-        if choice == 'login':
-            self.username = input('username: ')
-            self.password = input('password: ')
+        choice = None
+        while choice != 'q':
+            choice = input('welcome to fitness db \nlogin(l), create user(c), or quit(q) ')
+            choice.lower()
+            if choice == 'l':
+                count = 0
+                while count <= 3:
+                    self.username = input('username: ')
+                    self.password = input('password: ')
+                    if self.get_credentials():
+                        print(f'welcome back {self.username}')
+                        self.main_menu()
+                    elif count == 3:
+                        username = input('username: ')
+                        password = input('password: ')
+                        first_name = input('first name: ')
+                        last_name = input('last name: ')
+                        
+                        values1 = (username, password, first_name, last_name)
+                        self.cursor.execute('INSERT INTO users(user_name, password, first_name, last_name) VALUES (?, ?, ?, ?)', values1)
+                        self.connection.commit()
+                        self.main_menu()
+                    else:
+                        print('try again\n')
+                        count += 1
 
-            values = (self.username,)
-            db_user, db_pass = self.get_credentials(values)
-            
-            if db_user == self.username and db_pass == self.password:
-                print(f'welcome back {self.username}')
+            elif choice == 'c':
+                print('create new user')
+                username = input('username: ')
+                password = input('password: ')
+                first_name = input('first name: ')
+                last_name = input('last name: ')
+                
+                values1 = (username, password, first_name, last_name)
+                self.cursor.execute('INSERT INTO users(user_name, password, first_name, last_name) VALUES (?, ?, ?, ?)', values1)
+                self.connection.commit()
                 self.main_menu()
+            elif choice == 'q':
+                choice = 'q'
             else:
-                print('incorrect credentials')
+                print('incorrect response\n')
 
-        elif choice == 'create':
-            username = input('username: ')
-            password = input('password: ')
-            first_name = input('first name: ')
-            last_name = input('last name: ')
-            
-            values1 = (username, password, first_name, last_name)
-            self.cursor.execute('INSERT INTO users(user_name, password, first_name, last_name) VALUES (?, ?, ?, ?)', values1)
-            self.connection.commit()
-            # self.connection.close()
-            self.main_menu()
-
-    
     def add_workout(self):
-        choice = input('did you workout today? ')
-        choice.lower()
+        choice = None
         while choice != 'n':
+            choice = input('did you workout today?(y or n) ')
+            choice.lower()
             if choice == 'y':
                 exercise = input('exercise: ')
                 sets = input('sets: ')
                 reps = input('reps: ')
                 weight = input('weight: ')
-                self.cursor.execute("SELECT exercise_name FROM exercises WHERE exercise_name = (?)", exercise)
+                value_exer = (exercise, )
+                self.cursor.execute("SELECT exercise_name FROM exercises WHERE exercise_name = ?", value_exer)
                 if self.cursor.fetchall():
                         values = (self.username, exercise, sets, reps, weight)
                         self.cursor.execute('''
                         INSERT INTO user_workouts(date, user_id, exercise_id, sets, reps, weight) VALUES 
-                        (
-                            (SELECT DATETIME())
-                        ,   (SELECT user_id FROM users WHERE user_name = ?)
-                        ,   (SELECT exercise_id FROM exercises WHERE exercise_name = ?)
-                        ,   ?
-                        ,   ?
-                        ,   ?
-                        )''', values)
+                            (
+                                (SELECT DATE()), 
+                                (SELECT user_id FROM users WHERE user_name = ?), 
+                                (SELECT exercise_id FROM exercises WHERE exercise_name = ?),
+                                ?,
+                                ?,
+                                ?
+                            )''', values)
                         self.connection.commit()
                         choice = input('another exercise? ')
                         choice.lower()
@@ -80,7 +97,7 @@ class user():
                     self.add_exercise()    
         if choice == 'n':
             print('you are lazy')
-
+            # choice = 'n'
 
     def add_exercise(self):
         choice = input('add an exercise? ')
@@ -89,29 +106,32 @@ class user():
             exercise = input('what is the name of the exercise? ')
             descri = input('enter a description: ')
             values = exercise, descri
-            self.cursor.execute('INSERT INTO exercises (exercise_name, description) VALUES ?, ?', values)
+            self.cursor.execute('INSERT INTO exercises (exercise_name, description) VALUES (?, ?)', values)
+            print('the exercise was added to the database ')
             self.connection.commit()
         else:
-            self.main_menu()
-
+            ('returning to main menu')
 
     def view_workout(self):
         # view exercise_name, sets, reps, weight where the username = 
-        date = input('enter the date of the workout you would like to see' )
+        date = input('enter the date of the workout you would like to see (YYYY-MM-DD) ' )
+        values = (self.username, date)
         self.cursor.execute('''SELECT exercise_name, sets, reps, weight 
             FROM user_workouts 
                 JOIN exercises
                     ON user_workouts.exercise_id = exercises.exercise_id
                 JOIN users
                     ON users.user_id = exercises.exercise_id
-            WHERE users.user_id =  ?
-            ''', self.username)
-        print(self.cursor.fetchall())
+            WHERE users.user_id =  ? and date = ?
+            ''', values)
+        results = self.cursor.fetchall()
+        print(results)
 
     def main_menu(self):
         # option of viewing, adding workout or exercise
-        choice = input('what would you like to do?: view workout(v), add workout(w), add exercise(e), or quit(q)')
+        choice = None
         while choice != 'q':
+            choice = input('what would you like to do?: view workout(v), add workout(w), add exercise(e), or quit(q) ')
             if choice == 'v':
                 self.view_workout()
             elif choice == 'w':
@@ -119,9 +139,7 @@ class user():
             elif choice == 'e':
                 self.add_exercise()
             elif choice == 'q':
-                break
+                quit()
             else:
                 print('must choose v, w, e, or q')
-
-
 
